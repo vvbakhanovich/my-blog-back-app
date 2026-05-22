43243e2f9c82d210b509f5a3ed479dbfb5213614
Bakhanovich Vladimir <v.bakhanovich@arenadc.io>
2026-05-22 15:59:11 +0300

    remove PostMapper from PostController

--- src/main/java/dev/vvbakh/posts/PostController.java
+++ src/main/java/dev/vvbakh/posts/PostController.java
@@ -1,11 +1,11 @@
 package dev.vvbakh.posts;
 
-import dev.vvbakh.posts.mapper.PostMapper;
 import dev.vvbakh.posts.dto.CreatePostDto;
-import dev.vvbakh.posts.model.Post;
 import dev.vvbakh.posts.dto.PostDto;
 import dev.vvbakh.posts.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
-    private final PostMapper postMapper;
 
     @PostMapping
     public PostDto createPost(@RequestBody CreatePostDto createPostDto) {
-        final Post newPost = postMapper.toModel(createPostDto);
-        final Post createdPost = postService.create(newPost);
-        return postMapper.toDto(createdPost);
+        return postService.create(createPostDto);
+    }
+
+    @GetMapping("/{postId}")
+    public PostDto getPostById(@PathVariable long postId) {
+        return postService.getById(postId);
     }
 }
