f814161b7da79d9645d859bcfdfd778242ec36ac
Bakhanovich Vladimir <v.bakhanovich@arenadc.io>
2026-05-26 17:30:29 +0300

    feat: configure multipart support via StandardServletMultipartResolver and web.xml

--- src/main/java/dev/vvbakh/config/MultipartConfiguration.java
+++ src/main/java/dev/vvbakh/config/MultipartConfiguration.java
@@ -1,4 +1,15 @@
 package dev.vvbakh.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+@Configuration
 public class MultipartConfiguration {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 }
