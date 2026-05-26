9afdec12bfdd683f3bbbc99cfd240ff0eb7e70e6
Bakhanovich Vladimir <v.bakhanovich@arenadc.io>
2026-05-26 18:27:05 +0300

    feat: add CommentsService interface and CommentsServiceImpl

--- src/main/java/dev/vvbakh/comments/service/CommentsService.java
+++ src/main/java/dev/vvbakh/comments/service/CommentsService.java
@@ -1,4 +1,17 @@
-package dev.vvbakh.comments;
+package dev.vvbakh.comments.service;
+
+import dev.vvbakh.comments.dto.CommentDto;
+import dev.vvbakh.comments.dto.CreateCommentDto;
+
+import java.util.List;
 
 public interface CommentsService {
+
+    CommentDto add(long postId, CreateCommentDto dto);
+
+    List<CommentDto> getAll(long postId);
+
+    CommentDto update(long postId, long commentId, CreateCommentDto dto);
+
+    void delete(long postId, long commentId);
 }
