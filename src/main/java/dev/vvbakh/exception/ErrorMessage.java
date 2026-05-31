379bc8e7ff5e99c0676f8caffebf3d747be92a97
Bakhanovich Vladimir <v.bakhanovich@arenadc.io>
2026-05-31 13:44:23 +0300

    refactor: remove unused statusCode from ErrorMessage, add InvalidFileTypeException handler

--- src/main/java/dev/vvbakh/exception/ErrorMessage.java
+++ src/main/java/dev/vvbakh/exception/ErrorMessage.java
@@ -2,5 +2,5 @@ package dev.vvbakh.exception;
 
 import java.util.Map;
 
-public record ErrorMessage(Map<String, String> errors, int statusCode) {
+public record ErrorMessage(Map<String, String> errors) {
 }
